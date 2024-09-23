5f03ad306d0eb4c3fffec156e4510e7e350fdffd
nayeem98746 <sabbirahmednayeem941@gmail.com>
2024-09-23 23:55:23 +0600

    changes

--- DataSetStatic/common/Home_page/Data_home.js
+++ DataSetStatic/common/Home_page/Data_home.js
@@ -71,7 +71,98 @@ export const FeaturedCategoriesStatic = [
         slug:"Headphone",
         img: "/images/headphone-64.png",
     },
-   
-    
     
 ]
+
+export const laptopData = [
+    {
+        name: "ASUS TUF GAMING A15 FA507RM",
+        display: "15.6-inch (39.62 cms) FHD 144Hz",
+        processor: "AMD Ryzen 7 5800H",
+        ram: "8GB/16GB",
+        storage: "512GB SSD",
+        graphics: "NVIDIA GeForce GTX 1660 Ti 6GB",
+        brand: "ASUS"
+    },
+    {
+        name: "Dell XPS 13 9310",
+        display: "13.4-inch (33.78 cms) UHD+ InfinityEdge Touch Display",
+        processor: "Intel Core i7-1165G7",
+        ram: "16GB",
+        storage: "512GB SSD",
+        graphics: "Intel Iris Xe Graphics",
+        brand: "Dell"
+    },
+    {
+        name: "HP Spectre x360 14",
+        display: "13.5-inch (34.29 cms) OLED 3K2K Touch Display",
+        processor: "Intel Core i7-1355U",
+        ram: "16GB",
+        storage: "512GB SSD",
+        graphics: "Intel Iris Xe Graphics",
+        brand: "HP"
+    },
+    {
+        name: "Lenovo Legion 5 Pro",
+        display: "16-inch (40.64 cms) WQXGA 165Hz",
+        processor: "AMD Ryzen 7 6800H",
+        ram: "16GB",
+        storage: "1TB SSD",
+        graphics: "NVIDIA GeForce RTX 3070 8GB",
+        brand: "Lenovo"
+    },
+    {
+        name: "Apple MacBook Pro 16 M2 Max",
+        display: "16-inch (40.64 cms) Liquid Retina XDR",
+        processor: "Apple M2 Max",
+        ram: "32GB",
+        storage: "1TB SSD",
+        graphics: "Apple M2 Max GPU (38-core)",
+        brand: "Apple"
+    },
+    {
+        name: "Acer Nitro 5",
+        display: "15.6-inch (39.62 cms) FHD 144Hz",
+        processor: "Intel Core i5-12500H",
+        ram: "16GB",
+        storage: "512GB SSD",
+        graphics: "NVIDIA GeForce RTX 3060 6GB",
+        brand: "Acer"
+    },
+    {
+        name: "MSI GF63 Thin",
+        display: "15.6-inch (39.62 cms) FHD 144Hz",
+        processor: "Intel Core i5-12450H",
+        ram: "8GB",
+        storage: "256GB SSD",
+        graphics: "NVIDIA GeForce GTX 1650 4GB",
+        brand: "MSI"
+    },
+    {
+        name: "Razer Blade 15",
+        display: "15.6-inch (39.62 cms) QHD 240Hz",
+        processor: "Intel Core i7-12800H",
+        ram: "16GB",
+        storage: "1TB SSD",
+        graphics: "NVIDIA GeForce RTX 3080 Ti 8GB",
+        brand: "Razer"
+    },
+    {
+        name: "Microsoft Surface Laptop 5",
+        display: "13.5-inch (34.29 cms) PixelSense Display",
+        processor: "Intel Core i7-1265U",
+        ram: "16GB",
+        storage: "512GB SSD",
+        graphics: "Intel Iris Xe Graphics",
+        brand: "Microsoft"
+    },
+    {
+        name: "Gigabyte Aero 16",
+        display: "16-inch (40.64 cms) 4K UHD OLED",
+        processor: "Intel Core i9-12900HK",
+        ram: "32GB",
+        storage: "1TB SSD",
+        graphics: "NVIDIA GeForce RTX 3080 Ti 16GB",
+        brand: "Gigabyte"
+    }
+];
