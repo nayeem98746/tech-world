eb4fbb71ea93197c2d5236ffaaea696183d8ec89
nayeem98746 <sabbirahmednayeem941@gmail.com>
2024-09-26 23:57:44 +0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
 import MainLayout from '../Components/MainLayout/MainLayout'
 import { home_pageMeta } from '../DataSetStatic/HomePage/data_home'
  import HomeCarosel from '../Components/HomePageComponent/HomeCarosel'
